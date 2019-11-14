9b8f86db515d6922480ba677cad2512e56297391
asalimil <43479233+asalimil@users.noreply.github.com>
2019-11-14 14:26:43 -0700

    Update schur_stability.m

--- schur_stability.m
+++ schur_stability.m
@@ -1,1 +1,24 @@
+%% LMI for Matrix Norm Minimization
+clc; clear; close all;
 
+A = [0  0   1  0
+     0  0   0  1
+    -0.5  0  -2.5  1
+     0 -0.5   1 -2];
+
+B = [0 0
+     0 0
+     1 0
+     0 1];
+
+gamma = sdpvar(1);
+K = sdpvar(size(B,2),size(A,1));
+
+C = [];
+eta = 0.00001;
+M = [-gamma*eye(size(A,1))    (A + B*K)
+      (A + B*K)'      -gamma*eye(size(A,1))];
+F = [C, M <= eta*eye(size(M))];
+optimize(F,gamma);
+gamma = value(gamma)
+K = value(K)
